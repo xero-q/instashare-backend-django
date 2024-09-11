e6f56b24005c035a1c6fb116512640c956aa78e6
Anibal Sanchez <anibal.sanchez@dspot.com.pl>
2024-09-11 12:33:25 -0400

    Adding new packages. Adding Swagger

--- djangobackend/settings.py
+++ djangobackend/settings.py
@@ -50,7 +50,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-     'rest_framework_simplejwt'
+     'rest_framework_simplejwt',
+     'drf_yasg'
 ]
 
 MIDDLEWARE = [
