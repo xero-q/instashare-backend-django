805126c50c2eeea324d6d5a2cb0a8f9634fb76eb
Anibal Sanchez <anibal.sanchez@dspot.com.pl>
2024-09-12 08:19:34 -0400

    Cleaning code

--- files/views.py
+++ files/views.py
@@ -4,7 +4,6 @@ from rest_framework.parsers import MultiPartParser
 from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
 from rest_framework import status
-from rest_framework.exceptions import MethodNotAllowed
 from .serializers import UserSerializer,UploadedFileSerializer
 from .models import UploadedFile
 from django.contrib.auth.models import User
