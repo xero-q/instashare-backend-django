a1c29455b4a94ac99f5ceff97e785354afb1a3c0
Anibal Sanchez <anibal.sanchez@dspot.com.pl>
2024-09-12 22:00:12 -0400

    Setting token expiration in 15 minutes

--- djangobackend/settings.py
+++ djangobackend/settings.py
@@ -159,7 +159,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
