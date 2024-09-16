c8f1681839359d66fed03f3c83e8852a523bdadb
Anibal Sanchez <anibal.sanchez@dspot.com.pl>
2024-09-16 12:20:59 -0400

    Fixing compression of files

--- files/tasks.py
+++ files/tasks.py
@@ -13,16 +13,19 @@ def compress_files():
         file_real_name = file_real_name_parts[-1]
         
         file_path = os.path.join(settings.BASE_DIR, settings.UPLOADS_FOLDER, file_real_name)
-        compressed_file_path = os.path.join(settings.BASE_DIR, settings.UPLOADS_FOLDER, f"{file_real_name}.zip")       
+        compressed_file_path = os.path.join(settings.BASE_DIR, settings.UPLOADS_FOLDER, f"{file_real_name}.zip")
 
          # Create a zip file for the uncompressed file
-        with zipfile.ZipFile(compressed_file_path, 'w') as zipf:
+        with zipfile.ZipFile(compressed_file_path, 'w',compression=zipfile.ZIP_DEFLATED) as zipf:
             zipf.write(file_path, os.path.basename(file_path))
+
+        new_file_size = os.path.getsize(compressed_file_path) 
 
         # Replace original file with the compressed file in the database
         file_record.name = f"{file_real_name}.zip"
         file_real_name_parts[-1] = f"{file_real_name}.zip"
         file_record.file.name = '/'.join(file_real_name_parts)
+        file_record.size = new_file_size
         file_record.status = 'compressed'
         
         file_record.save()
