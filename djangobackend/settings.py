a78b0505de4d8d011dcb4de363614656551ba99e
Anibal Sanchez <anibalnuma@gmail.com>
2025-05-23 18:14:59 -0400

    Update settings

--- djangobackend/settings.py
+++ djangobackend/settings.py
@@ -105,12 +105,6 @@ DATABASES = {
     }
 }
 
-if not DEBUG:
-    DATABASES["default"] = dj_database_url.config(
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
